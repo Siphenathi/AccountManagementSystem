e818ab748c8a07ef49ec9255b37679b4c668ca2a
Siphenathi <spantshwa.lukho@gmail.com>
2021-04-08 00:55:23 +0200

    UI initial commit
    This reverts commit 401c3714364888f4a694db28d430a5b963a92d4a.

---
+++ account-management-system-ui/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from '../home/home.component';
+
+const routes: Routes = [
+  {path: 'home', component: null},
+  {path: '', redirectTo: '/home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
