5e85dd42de59e98ec8866270c4bd1b9dd463cf4a
Siphenathi <spantshwa.lukho@gmail.com>
2021-04-08 02:16:51 +0200

    wip

--- account-management-system-ui/src/app/app-routing.module.ts
+++ account-management-system-ui/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 // import { HomeComponent } from '../home/home.component';
 
 const routes: Routes = [
-  {path: 'home', component: null},
-  {path: '', redirectTo: '/home', pathMatch: 'full' }
+  // {path: 'home', component: null},
+  // {path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [
-    CommonModule,
+    //CommonModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
-  declarations: []
+  //declarations: []
 })
 export class AppRoutingModule { }
--- account-management-system-ui/src/app/app.module.ts
+++ account-management-system-ui/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
